6bf5d4534cd160defa1772c79168c6cd239c029d
Sandeep <samudsan2020@gmail.com>
2022-11-14 05:33:51 +0530

    config.properties added: now properties are loading from config.properties

--- src/test/java/com/san/tests/HomePageTests.java
+++ src/test/java/com/san/tests/HomePageTests.java
@@ -1,8 +1,6 @@
 package com.san.tests;
 
-import com.san.driver.Driver;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
+
 import org.testng.annotations.Test;
 
 public final class HomePageTests extends BaseTest{
